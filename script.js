b00b29a075dbc8fd070005cb0dfd5831ce48827f
Manon <manonlefkos@gmail.com>
2023-03-02 16:12:49 +0100

    add squares renderer with contents from array

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
+/* eslint-disable no-underscore-dangle */
 // gameBoard Module
 const gameBoard = (function () {
-  const _gameBoardContents = ["X", "O", "X", "O", "X", "O", "X", "<3"];
+  const _gameBoardContents = ["X", "O", "X", "O", "X", "O", "X", "O", "<3"];
+  // gameboard creator
+  const _container = document.querySelector(".gameBoardContainer");
   // render
-  function showGame() {
-    for (let i = 0; i <= _gameBoardContents.length; i++) {
-      console.log(_gameBoardContents[i]);
+  function _renderSquares(arrayItem) {
+    const square = document.createElement("div");
+      square.setAttribute("data-value:", [arrayItem]);
+      square.textContent = (_gameBoardContents[arrayItem])
+      _container.appendChild(square);
+  }
+  function showMoves() {
+    for (let i = 0; i < _gameBoardContents.length; i++) {
+      _renderSquares(i);
     }
   }
   // returned
   return {
-    showGame,
+    showMoves,
   };
 })();
