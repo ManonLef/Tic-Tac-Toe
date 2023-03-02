9c0023cea89f4a1a881f8e1031f93746975cafa9
Manon <manonlefkos@gmail.com>
2023-03-02 16:22:45 +0100

    add button renderer for testing

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 /* eslint-disable no-underscore-dangle */
 // gameBoard Module
 const gameBoard = (function () {
-  const _gameBoardContents = ["X", "O", "X", "O", "X", "O", "X", "O", "<3"];
+  const _gameBoardContents = ["X", "O", "", "O", "X", "O", "X", "O", "<3"];
+
   // gameboard creator
   const _container = document.querySelector(".gameBoardContainer");
+
+  //listeners
+  document.querySelector("button").addEventListener("click", showMoves)
   // render
   function _renderSquares(arrayItem) {
     const square = document.createElement("div");
-      square.setAttribute("data-value:", [arrayItem]);
-      square.textContent = (_gameBoardContents[arrayItem])
-      _container.appendChild(square);
+    square.setAttribute("data-value", [arrayItem]);
+    square.textContent = _gameBoardContents[arrayItem];
+    _container.appendChild(square);
   }
   function showMoves() {
     for (let i = 0; i < _gameBoardContents.length; i++) {
